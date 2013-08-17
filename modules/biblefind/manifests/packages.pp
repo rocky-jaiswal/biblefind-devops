1f087cc540558dbf2486cf141d31466d53685b12
Rocky Jaiswal <rocky.jaiswal@gmail.com>
2013-08-17 14:40:01 +0530

    Fix - 9

--- modules/biblefind/manifests/packages.pp
+++ modules/biblefind/manifests/packages.pp
@@ -6,15 +6,6 @@ class biblefind::packages {
 
   class mongodb
 
-  apt::ppa { "ppa:chris-lea/node.js": 
-    before  => Package["nodejs"],
-  }
-
-  package { "nodejs": 
-    ensure => latest,
-    require => Package["python-software-properties"]
-  }
-
   package { "openjdk-7-jdk": 
     ensure => latest,
   }
