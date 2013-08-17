88ffdef66a7323a8e28f1cf6e201adc111cad504
Rocky Jaiswal <rocky.jaiswal@gmail.com>
2013-08-17 14:43:03 +0530

    removing mdb module

--- modules/biblefind/manifests/packages.pp
+++ modules/biblefind/manifests/packages.pp
@@ -2,11 +2,6 @@ class biblefind::packages {
   
   include stdlib
   include apt
-  include mongodb
-
-  class mongodb {
-    init => 'sysv',
-  }
 
   package { "openjdk-7-jdk": 
     ensure => latest,
