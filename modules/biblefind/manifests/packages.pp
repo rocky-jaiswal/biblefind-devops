40855679cdf16579c0d1b9e3b59b22efb1713bf4
Rocky Jaiswal <rocky.jaiswal@gmail.com>
2013-08-17 14:23:26 +0530

    Fix - 5

--- modules/biblefind/manifests/packages.pp
+++ modules/biblefind/manifests/packages.pp
@@ -3,6 +3,10 @@ class biblefind::packages {
   include stdlib
   include apt
 
+  class { 'mongodb': 
+    use_10gen => true,
+  }
+
   apt::ppa { "ppa:chris-lea/node.js": 
     before  => Package["nodejs"],
   }
